f33028b3053da219b5734c7496e364364fae6a79
arashalaei <arashalaei22@gmail.com>
2024-12-17 00:13:06 +0330

    update the signup example

--- examples/signup.py
+++ examples/signup.py
@@ -1,34 +1,31 @@
-from zarban.wallet.openapi_client.configuration import Configuration
-from zarban.wallet.openapi_client.api_client import ApiClient
-from zarban.wallet.openapi_client.api import DefaultApi
-from zarban.wallet.openapi_client.models import SignUpRequest
-from zarban.wallet.openapi_client.exceptions import ApiException
+import zarban.wallet.openapi_client as wallet
+
 
 def signup_example():
     # Create and configure the Configuration object
-    configuration = Configuration(
+    cfg = wallet.Configuration(
         host="https://testwapi.zarban.io"
     )
 
     # Create an instance of the ApiClient with the configuration
-    api_client = ApiClient(configuration)
+    api_client = wallet.ApiClient(configuration=cfg)
 
-    # Create an instance of the DefaultApi using the ApiClient
-    api_instance = DefaultApi(api_client)
+    # Create an instance of the AuthApi using the api_client
+    auth_api = wallet.AuthApi(api_client)
 
     # Prepare the signup request data
-    signup_request = SignUpRequest(
+    signup_request = wallet.SignUpRequest(
         email="user@example.com",
         password="yourSecuredPassword",
     )
 
     try:
         # Call the signup API
-        api_response = api_instance.auth_signup_post(signup_request)
-        print("Confirmation link sent successful!")
+        api_response = auth_api.signup_with_email_and_password(signup_request)
+        print("Confirmation link sent successfully!")
         print(f"Message: {api_response.messages}")
 
-    except ApiException as e:
+    except wallet.exceptions.ApiException:
         print(f"Exception when calling DefaultApi->auth_signup_post: {e}")
         print(f"Error message: {e.body}")
 
